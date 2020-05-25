1dbcbd4206e9e1bc854e62235383d62c8d64c63e
r3code <contact@r3code.ru>
2020-05-25 22:15:08 +0300

    chore: added exite code 1 at error

--- cmd/hlfqueue/main.go
+++ cmd/hlfqueue/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"os"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 	hlfq "github.com/r3code/hlf-queue-example"
@@ -11,6 +12,7 @@ func main() {
 	cc := hlfq.New()
 	if err := shim.Start(cc); err != nil {
 		fmt.Printf("Error starting HLFQueue chaincode: %s", err)
+		os.Exit(1)
 	}
 
 }
