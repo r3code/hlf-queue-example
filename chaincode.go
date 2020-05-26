0db208b51384dc3b89d8747d8b3c9e5bd72048e5
r3code <contact@r3code.ru>
2020-05-26 10:23:23 +0300

    refac: AttachData moved to separate file

--- chaincode.go
+++ chaincode.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 
 	"github.com/antonmedv/expr"
-	"github.com/oklog/ulid/v2"
 	"github.com/pkg/errors"
 	"github.com/s7techlab/cckit/extensions/debug"
 	"github.com/s7techlab/cckit/extensions/owner"
@@ -45,31 +44,6 @@ const (
 	initQueueNameParam     = "queueName"
 	attachedDataParam      = "attachedData"
 )
-
-// attcahes extra data to the item specified by key, returns error if key not exists
-// arg1 -> attachDataMethodParamKey string
-// arg2 -> attachDataMethodParamData []bytes
-func queueAttachData(c router.Context) (interface{}, error) {
-	idStr := c.ParamString(itemIDParam)
-	id, err := ulid.ParseStrict(idStr)
-	if err != nil {
-		return nil, errors.Wrap(err, "invalid ULID string passed")
-	}
-	extraData := c.ParamBytes(attachedDataParam)
-	queryItem := &QueueItem{ID: id}
-	key, _ := queryItem.Key() // always no error
-	item, err := readQueueItem(c, key)
-	if err != nil {
-		return nil, errors.Wrap(err, "can not read item to attach data")
-	}
-	item.ExtraData = []byte{} // reset
-	item.ExtraData = append(item.ExtraData, extraData...)
-	// fmt.Printf("\n\n***** item=%+v\n\n", item)
-	if err := c.State().Put(item); err != nil {
-		return nil, errors.Wrap(err, "failed to update item with extra data")
-	}
-	return item, nil
-}
 
 // returns error if itemID or afterItemID not exists
 // arg1 -> itemID string (ULID String)
---
+++ chaincode_attach_data.go
@@ -0,0 +1,32 @@
+package hlfq
+
+import (
+	"github.com/oklog/ulid/v2"
+	"github.com/pkg/errors"
+	"github.com/s7techlab/cckit/router"
+)
+
+// attcahes extra data to the item specified by key, returns error if key not exists
+// arg1 -> attachDataMethodParamKey string
+// arg2 -> attachDataMethodParamData []bytes
+func queueAttachData(c router.Context) (interface{}, error) {
+	idStr := c.ParamString(itemIDParam)
+	id, err := ulid.ParseStrict(idStr)
+	if err != nil {
+		return nil, errors.Wrap(err, "invalid ULID string passed")
+	}
+	extraData := c.ParamBytes(attachedDataParam)
+	queryItem := &QueueItem{ID: id}
+	key, _ := queryItem.Key() // always no error
+	item, err := readQueueItem(c, key)
+	if err != nil {
+		return nil, errors.Wrap(err, "can not read item to attach data")
+	}
+	item.ExtraData = []byte{} // reset
+	item.ExtraData = append(item.ExtraData, extraData...)
+	// fmt.Printf("\n\n***** item=%+v\n\n", item)
+	if err := c.State().Put(item); err != nil {
+		return nil, errors.Wrap(err, "failed to update item with extra data")
+	}
+	return item, nil
+}
